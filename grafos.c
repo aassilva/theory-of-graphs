bd76324f9ed976e6b4a9b28438d4c87f3235ca81
antlis <antonio.silva27@outlook.com>
2017-11-10 20:01:47 -0200

    Update

--- grafos.c
+++ grafos.c
@@ -5,15 +5,23 @@
 
 int num_nodes = 0;
 
-
-
+//Estrutura de armazenamento para armazenar o grafo.
 typedef struct no {
 	int vertice;
+	int peso;
 	struct no *prox;
 } nodes;
 
 nodes list[10000];
 
+//Estrutura de armazenamento para a implementação das buscas.
+typedef struct Node{
+	int num;
+	struct Node *prox;
+} lista_busca;
+
+
+//Códigos para a geração e armazenamento do grafo simples.
 int tamanho (int num_nodes) {
 	printf("\n");
 	printf("Entre com a quantidade de vertices: ");
@@ -128,8 +136,44 @@ void exibir_list(nodes *lista, int num_nodes) {
 	}
 }
 
+//Código para as buscas.
+void insere_fila(lista_busca *fila, int raiz){
+	lista_busca *novo = (lista_busca*) malloc((int)sizeof(lista_busca));
+	novo->prox = NULL;
+	novo->num = raiz;
+	lista_busca *tmp = fila->prox;
 
-void busca_grafo_simples(nodes list[num_nodes],int num_nodes,float sparsity) {
+	while (tmp->prox != NULL) {
+		tmp = tmp->prox;
+	}
+
+	tmp->prox = novo;
+}
+
+void bfs_busca(nodes list[num_nodes], int num_nodes, int raiz) {
+	lista_busca *fila;
+	fila = (lista_busca*) malloc((int)sizeof(lista_busca));
+	int matriz[num_nodes][2], vertice_selecionado;
+	for (int linha = 0; linha < num_nodes; linha++) {
+		for (int coluna = 0; coluna < 2; coluna++) {
+			matriz[linha][coluna] = 0;
+		}
+	}
+	fila->prox = NULL;
+	
+	insere_fila(fila, raiz);
+	matriz[raiz][1] = 1;  //teg.
+	matriz[raiz][2] = -1; //antecessor.
+
+	/*while (!vazia(FILA)) {
+		vertice-selecionado = FILA->num;
+
+	}*/
+	
+}
+
+
+void busca_grafo_simples(nodes list[num_nodes],int num_nodes) {
 	int opcao;
 	printf("\n");
 	printf("1. exibir o grafo. \n2. para BFS. \n3. para DFS. \n0. para sair.\nDigite uma das opções acima: ");
@@ -140,8 +184,12 @@ void busca_grafo_simples(nodes list[num_nodes],int num_nodes,float sparsity) {
 				exibir_list(list, num_nodes);
 			}
 			if (opcao == 2) {
+				int raiz;
 				printf("\n\nGrafo originalmente armazenado.\n\n");
 				exibir_list(list, num_nodes);
+				printf("Escolha o nó raiz: ");
+				scanf("%d", &raiz);
+				bfs_busca(list, num_nodes, raiz);
 				//BFS
 			}
 			if (opcao == 3) {
@@ -152,6 +200,10 @@ void busca_grafo_simples(nodes list[num_nodes],int num_nodes,float sparsity) {
 		}
 	} while (opcao == 0);
 }
+
+
+
+
 
 int main() {
 	srand((unsigned int)time(NULL));
@@ -178,7 +230,7 @@ int main() {
 						list[i].prox = NULL;
 					}
 					create_list(list, num_nodes, sparsity);
-					busca_grafo_simples(list, num_nodes, sparsity);
+					busca_grafo_simples(list, num_nodes);
 				}
 				if (opcao == 0) {
 					goto inicio;
